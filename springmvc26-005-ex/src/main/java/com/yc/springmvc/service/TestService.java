c939011f767e3b217cde8154d91c1e486dab95d3
milul <1014789568@qq.com>
2016-12-21 21:11:51 +0800

    添加一个工程 	--milul

---
+++ springmvc26-005-ex/src/main/java/com/yc/springmvc/service/TestService.java
@@ -0,0 +1,7 @@
+package com.yc.springmvc.service;
+
+public interface TestService {  
+    public void exception(Integer id) throws Exception;  
+      
+    public void dao(Integer id) throws Exception;  
+}  
---
+++ springmvc26-005-ex/src/main/java/com/yc/springmvc/service/impl/TestServiceImpl.java
@@ -0,0 +1,38 @@
+package com.yc.springmvc.service.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.yc.springmvc.dao.TestDao;
+import com.yc.springmvc.exception.BusinessException;
+import com.yc.springmvc.exception.ParameterException;
+import com.yc.springmvc.service.TestService;
+
+@Service("testService")  
+public class TestServiceImpl implements TestService {  
+    @Resource  
+    private TestDao testDao;  
+      
+    public void exception(Integer id) throws Exception {  
+        switch(id) {  
+        case 1:  
+            throw new BusinessException("11", "service11");  
+        case 2:  
+            throw new BusinessException("21", "service21");  
+        case 3:  
+            throw new BusinessException("31", "service31");  
+        case 4:  
+            throw new BusinessException("41", "service41");  
+        case 5:  
+            throw new BusinessException("51", "service51");  
+        default:  
+            throw new ParameterException("Service Parameter Error");  
+        }  
+    }  
+  
+    @Override  
+    public void dao(Integer id) throws Exception {  
+        testDao.exception(id);  
+    }  
+}  
